771ad1f868c5d1889679ec122aefe886bdee690b
Joseph Jin <jerkjoe61@gmail.com>
2020-05-19 10:17:57 -0700

    HRT-1598

--- fpo-web/src/app/hrt-group-statistical-information-page/hrt-group-statistical-information-page.component.ts
+++ fpo-web/src/app/hrt-group-statistical-information-page/hrt-group-statistical-information-page.component.ts
@@ -76,6 +76,9 @@ export class HrtGroupStatisticalInformationPageComponent
             name: "Primary Language",
             hasOther: true,
             choices: [
+              "English",
+              "French",
+              "ASL",
               "Chinese Traditional",
               "Chinese Simplified",
               "Punjabi",
@@ -107,6 +110,7 @@ export class HrtGroupStatisticalInformationPageComponent
             ],
             showClearButton: true,
             otherText: "Other",
+            hasOther: true,
           },
           {
             type: "radiogroup",
