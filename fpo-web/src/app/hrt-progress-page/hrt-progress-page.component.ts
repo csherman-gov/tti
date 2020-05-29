48838e78190980e3d1a44dee8a266f85db5a44e2
Joseph Jin <jerkjoe61@gmail.com>
2020-05-29 15:27:08 -0700

    test

--- fpo-web/src/app/hrt-progress-page/hrt-progress-page.component.ts
+++ fpo-web/src/app/hrt-progress-page/hrt-progress-page.component.ts
@@ -1,126 +1,144 @@
-import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from "@angular/core";
 
 // import service
-import { MissionService } from '../mission.service';
-import { Subscription } from 'rxjs';
+import { MissionService } from "../mission.service";
+import { Subscription } from "rxjs";
 
-import { Router } from '@angular/router';
+import { Router } from "@angular/router";
 // data service
 import { GeneralDataService, UserInfo } from "../general-data.service";
 
 @Component({
-  selector: 'app-hrt-progress-page',
-  templateUrl: './hrt-progress-page.component.html',
-  styleUrls: ['./hrt-progress-page.component.scss']
+  selector: "app-hrt-progress-page",
+  templateUrl: "./hrt-progress-page.component.html",
+  styleUrls: ["./hrt-progress-page.component.scss"],
 })
 export class HrtProgressPageComponent implements OnInit, OnDestroy {
-    
-  result: boolean
+  result: boolean;
   get showLateComplaints() {
-    return this.result
+    return this.result;
   }
   subscription: Subscription;
-  formData: object = {}
-  completedSteps: number = 0
+  formData: object = {};
+  completedSteps: number = 0;
   steps = [
     {
-      name: 'Party information',
-      intro: '',
-      url: 'hrt/complainant',
-      short_name: 'complainant'
+      name: "Party information",
+      intro: "",
+      url: "hrt/complainant",
+      short_name: "complainant",
     },
     {
-      name: 'Area and Grounds',
-      intro: '',
-      url: 'hrt/area-and-grounds',
-      short_name: 'areaAndGrounds'
+      name: "Area and Grounds",
+      intro: "",
+      url: "hrt/area-and-grounds",
+      short_name: "areaAndGrounds",
     },
     {
-      name: 'Details of the Discrimination',
-      intro: '',
-      url: 'hrt/details-of-the-discrimination',
-      short_name: 'detailsOfDiscrimination'
+      name: "Details of the Discrimination",
+      intro: "",
+      url: "hrt/details-of-the-discrimination",
+      short_name: "detailsOfDiscrimination",
     },
     {
-      name: 'Time limit to make complaint',
-      intro: '',
-      url: 'hrt/file-in-time',
-      short_name: 'fileInTime'
+      name: "Time limit to make complaint",
+      intro: "",
+      url: "hrt/file-in-time",
+      short_name: "fileInTime",
     },
     {
-      name: 'Other Proceedings',
-      intro: '',
-      url: 'hrt/other-proceedings',
-      short_name: 'otherProceedings'
+      name: "Other Proceedings",
+      intro: "",
+      url: "hrt/other-proceedings",
+      short_name: "otherProceedings",
     },
     {
-      name: 'Remedies',
-      intro: '',
-      url: 'hrt/remedies',
-      short_name: 'remedies'
+      name: "Remedies",
+      intro: "",
+      url: "hrt/remedies",
+      short_name: "remedies",
     },
     {
-      name: 'Mediation',
-      intro: '',
-      url: 'hrt/mediation',
-      short_name: 'mediation'
+      name: "Mediation",
+      intro: "",
+      url: "hrt/mediation",
+      short_name: "mediation",
     },
     {
-      name: 'Demographic Information',
-      intro: 'This step is optional',
-      url: 'hrt/statistical-information',
-      short_name: 'statisticalInformation'
-    }
-  ]
+      name: "Demographic Information",
+      intro: "This step is optional",
+      url: "hrt/statistical-information",
+      short_name: "statisticalInformation",
+    },
+  ];
   get buttonClass() {
     return this.steps.reduce((acc, step) => {
-      if(step.short_name === 'statisticalInformation') {
-        return acc && true
+      if (step.short_name === "statisticalInformation") {
+        return acc && true;
       }
-      return acc && this.formData[step.short_name]
-    }, true) ? 'btn btn-default' : 'btn btn-inactive'
+      return acc && this.formData[step.short_name];
+    }, true)
+      ? "btn btn-default"
+      : "btn btn-inactive";
   }
-  user_id = ''
-  constructor(private missionService: MissionService, private router: Router, private dataService: GeneralDataService) {
+  user_id = "";
+  constructor(
+    private missionService: MissionService,
+    private router: Router,
+    private dataService: GeneralDataService
+  ) {
     this.subscription = missionService.missionAnnounced$.subscribe(
-        allFormData => {
-          console.log('allFormData', allFormData)
-          if (allFormData) {
-            this.formData = allFormData
-            for (let key in this.formData) {
-              if (key == 'home') {
-                continue;
-              }
-              this.completedSteps++
+      (allFormData) => {
+        console.log("allFormData", allFormData);
+        if (allFormData) {
+          this.formData = allFormData;
+          for (let key in this.formData) {
+            if (key == "home") {
+              continue;
             }
-            this.subscription.unsubscribe();
+            this.completedSteps++;
           }
-        });
-        console.log('this.formData: ', this.formData)
-        // if(this.formData.complainant) {}
-    this.dataService
-    .loadSurveyResultIndex("default", "primary", false)
-    .then(result => {
-        console.log('loadSurveyResultIndex success')
-        console.log('result: ', result)
-      // this._surveyIndex = result.result || [];
-      this.user_id = result.result[0].user_id
-      console.log(this.user_id)
-      console.log(result.result[0])
-      if (result.result[0]) {
-        this.missionService.confirmMission(result.result[0].result);
-        // missionService.announceMission(this.allFormData);
-        this.formData = result.result[0].result
+          this.subscription.unsubscribe();
+        }
       }
-    })
-    .catch(err => {
-        console.log('loadSurveyResultIndex fail')
-      // this._surveyIndex = [];
-    });
+    );
+    console.log("this.formData: ", this.formData);
+    // if(this.formData.complainant) {}
   }
 
   ngOnInit() {
-    console.log(this.formData)
+    console.log(this.formData);
+    if (Object.keys(this.formData).length < 1) {
+        console.log('loadData')
+      this.dataService
+        .loadSurveyResultIndex("default", "primary", false)
+        .then((result) => {
+          console.log("loadSurveyResultIndex success");
+          console.log("result: ", result);
+          // this._surveyIndex = result.result || [];
+          this.user_id = result.result[0].user_id;
+          console.log(this.user_id);
+          console.log(result.result[0]);
+          if (result.result[0]) {
+            this.missionService.confirmMission(result.result[0].result);
+            // missionService.announceMission(this.allFormData);
+            this.formData = result.result[0].result;
+
+            // this.formData = allFormData
+            this.completedSteps = 0;
+            for (let key in this.formData) {
+              if (key == "home") {
+                continue;
+              }
+              this.completedSteps++;
+            }
+          }
+        })
+        .catch((err) => {
+          console.log("loadSurveyResultIndex fail");
+          // this._surveyIndex = [];
+        });
+    }
   }
   ngOnDestroy() {
     // prevent memory leak when component destroyed
@@ -128,29 +146,32 @@ export class HrtProgressPageComponent implements OnInit, OnDestroy {
   }
 
   handleSave() {
-    this.dataService.saveSurveyResult("default", "primary", this.formData).then(res => {
-        console.log('save ok')
-        console.log(res)
-    })
+    this.dataService
+      .saveSurveyResult("default", "primary", this.formData)
+      .then((res) => {
+        console.log("save ok");
+        console.log(res);
+      });
   }
   handleClickEvent(event) {
-    console.log(event.target.dataset)
-    const url = event.target.dataset.url || 'hrt/progress'
+    console.log(event.target.dataset);
+    const url = event.target.dataset.url || "hrt/progress";
     // jump to that section
-    this.router.navigateByUrl(url)
-
+    this.router.navigateByUrl(url);
   }
   // Validate and Navigate to Review Page
   handleReviewEvent(event) {
     // this.router.navigateByUrl('hrt/review')
-    
-    if (this.steps.reduce((acc, step) => {
-      if(step.short_name === 'statisticalInformation') {
-        return acc && true
-      }
-      return acc && this.formData[step.short_name]
-    }, true)) {
-      this.router.navigateByUrl('hrt/review')
+
+    if (
+      this.steps.reduce((acc, step) => {
+        if (step.short_name === "statisticalInformation") {
+          return acc && true;
+        }
+        return acc && this.formData[step.short_name];
+      }, true)
+    ) {
+      this.router.navigateByUrl("hrt/review");
     }
   }
 }
